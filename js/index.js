07f4ad7cf56b72f1e3312d563fc5cd3577a15fc5
leemong13 <160309465+leemong13@users.noreply.github.com>
2024-02-27 23:12:53 +0900

    워들 구현방법 연습용
    html.css구현

---
+++ js/index.js
@@ -0,0 +1,13 @@
+//로직을 짜야함
+//카멜표기법:대문자작성.띄어쓰기대신임(ex.hiImIronMan)
+
+function appStart() {
+  const handleKeydown = (event) => {
+    const key = event.key;
+    const keyCode = event.keyCode;
+    //document.querySelector('bord')
+  };
+  window.addEventListener("keydown", handleKeydown);
+}
+
+appStart();
